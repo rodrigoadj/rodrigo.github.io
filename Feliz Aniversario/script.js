7a20ba93835c6b15a21197ce5de2d635f7ab6fb7
Rodrigo de Jesus <132757431+rodrigoadj@users.noreply.github.com>
2025-06-12 00:22:03 -0300

    Atualização da Pag

--- Feliz Aniversario/script.js
+++ Feliz Aniversario/script.js
@@ -8,7 +8,7 @@ function TrocarImg() {
         imagem.src = "pngtree-happy-birthday-vector-design-png-image_20334723.png";
         input.value = "Fechar presente";
     } else {
-        imagem.src = "christmas-gift-design-on-a-transparent-background-round-gift-box-design-with-pink-color-wrap-paper-and-golden-color-ribbon-gift-image-for-birthdays-anniversaries-weddings-or-christmas-events-free-png.webp";
+        imagem.src = "Feliz Aniversario/presente.png";
         input.value = "Abrir presente";
     }
 
