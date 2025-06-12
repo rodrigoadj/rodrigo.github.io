db88fa9172a18f6fad178da8b79de2eee1b796e5
Rodrigo de Jesus <132757431+rodrigoadj@users.noreply.github.com>
2025-06-12 00:26:35 -0300

    FEliz niver add

--- Feliz Aniversario/script.js
+++ Feliz Aniversario/script.js
@@ -5,7 +5,7 @@ function TrocarImg() {
     var imagem = document.getElementsByClassName("imagem")[0];
     var input = document.getElementsByClassName("buton")[0];
     if (statPresent) {
-        imagem.src = "Feliz Aniversario/pngtree-happy-birthday-vector-design-png-image_20334723.png";
+        imagem.src = "Feliz Aniversario/felizNiver.png";
         input.value = "Fechar presente";
     } else {
         imagem.src = "Feliz Aniversario/presente.png";
