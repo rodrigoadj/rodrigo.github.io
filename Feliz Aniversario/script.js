fa39b771b81e1322e18fc264d6aec7dff6547429
Rodrigo de Jesus <132757431+rodrigoadj@users.noreply.github.com>
2025-06-12 00:32:45 -0300

    ulitmo commit

--- Feliz Aniversario/script.js
+++ Feliz Aniversario/script.js
@@ -5,7 +5,7 @@ function TrocarImg() {
     var imagem = document.getElementsByClassName("imagem")[0];
     var input = document.getElementsByClassName("buton")[0];
     if (statPresent) {
-        imagem.src = "Feliz Aniversario/pngtree-balloon-frame-with-beautiful-sparkle-png-image_6217808.png";
+        imagem.src = "Feliz Aniversario/felizNiver.png";
         input.value = "Fechar presente";
     } else {
         imagem.src = "Feliz Aniversario/presente.png";
