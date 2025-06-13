234624fc23bf955c40cca3b859b1393ecf2a11b2
Rodrigo de Jesus <132757431+rodrigoadj@users.noreply.github.com>
2025-06-13 15:58:19 -0300

    Atualização

--- Feliz Aniversario/script.js
+++ Feliz Aniversario/script.js
@@ -5,11 +5,11 @@ function TrocarImg() {
     var imagem = document.getElementsByClassName("imagem")[0];
     var input = document.getElementsByClassName("buton")[0];
     if (statPresent) {
-        imagem.src = "Feliz Aniversario/felizNiver.png";
-        input.value = "Fechar presente";
+        imagem.src = "https://a-static.mlcdn.com.br/470x352/adesivo-decorativo-de-parede-frase-vai-ficar-tudo-bem-gaudesivos/lojaolist/olsy2z65t6ahetvb/0773b506244ba1706b9ee7f976af3f84.jpeg";
+        input.value = "Fechar Carta";
     } else {
-        imagem.src = "Feliz Aniversario/presente.png";
-        input.value = "Abrir presente";
+        imagem.src = "https://cdn-icons-png.flaticon.com/512/2331/2331211.png";
+        input.value = "Abrir Carta";
     }
 
 }
